68007c284e94d3b4d34cd76ccf01861934605a51
Antoine <pirlot.antoine@outlook.com>
2024-09-03 23:35:37 +0200

    Update: copyright
    Signed-off-by: Antoine <pirlot.antoine@outlook.com>

--- components/src/main/java/io/github/antoinepirlot/jetpack_libs/components/texts/Title.kt
+++ components/src/main/java/io/github/antoinepirlot/jetpack_libs/components/texts/Title.kt
@@ -1,15 +1,15 @@
 /*
- * This file is part of Satunes.
+ * This file is part of Jetpack-Libs.
  *
- *  Satunes is free software: you can redistribute it and/or modify it under
+ *  Jetpack-Libs is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free Software Foundation,
  *  either version 3 of the License, or (at your option) any later version.
  *
- *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  Jetpack-Libs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *  See the GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along with Satunes.
+ *  You should have received a copy of the GNU General Public License along with Jetpack-Libs.
  *  If not, see <https://www.gnu.org/licenses/>.
  *
  *  **** INFORMATIONS ABOUT THE AUTHOR *****
@@ -17,7 +17,7 @@
  *  You find this original project on github.
  *
  *  My github link is: https://github.com/antoinepirlot
- *  This current project's link is: https://github.com/antoinepirlot/Satunes
+ *  This current project's link is: https://github.com/antoinepirlot/Jetpack-Libs
  *
  *  You can contact me via my email: pirlot.antoine@outlook.com
  *  PS: I don't answer quickly.
